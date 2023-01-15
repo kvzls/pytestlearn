92581039c6bf115b41703b96f74ce5c18c50da2a
kvzls <122666621+kvzls@users.noreply.github.com>
2023-01-15 15:50:15 +0800

    Update all.py

--- all.py
+++ all.py
@@ -4,4 +4,4 @@ import pytest
 # if __name__ == '__main__':
 #     pytest.main()
 #     os.system("pytest --alluredir ./result --allure-features='qualityPlatform'")
-pytest.main(["--alluredir=result/allure-results", "--junitxml=result/xml-report/result.xml"])
+pytest.main(["--clean-alluredir", "--alluredir=result/allure-results", "--junitxml=result/xml-report/result.xml"])
